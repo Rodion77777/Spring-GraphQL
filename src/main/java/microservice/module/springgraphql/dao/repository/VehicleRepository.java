a47328d7947e1b99b8d14cabcc8134a3f18ae481
Rodion <rodionchik17@gmail.com>
2025-03-10 21:59:51 +0200

    bugfix

--- src/main/java/microservice/module/springgraphql/dao/repository/VehicleRepository.java
+++ src/main/java/microservice/module/springgraphql/dao/repository/VehicleRepository.java
@@ -2,6 +2,8 @@ package microservice.module.springgraphql.dao.repository;
 
 import microservice.module.springgraphql.dao.entity.Vehicle;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface VehicleRepository extends JpaRepository<Vehicle, Integer> {
 }
